4432c232268e6f44cc21acec35292b279cec1b9c
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2022-12-19 12:19:54 -0700

    TrackAdder - Add support for sound effects track
    - TrackAdder.py now allows you to specify a sound effects track that will be merged with each language track before being added to the video.
    - Also gives option for whether to save copies of the merged language+effects tracks

--- TrackAdder.py
+++ TrackAdder.py
@@ -3,19 +3,31 @@
 
 # REMEMBER: Unlike the .ini config files, the variable values here must be surrounded by "quotation" marks
 
-# The folder (or path relative to this script file) containing the audio track files to add to the video
-# Note: ALL audio tracks in the folder will be added, so ensure only the tracks you want are in there
-# A resulting copy of the original video, now with all tracks added, will also be placed in this folder
+    # The folder (or path relative to this script file) containing the audio track files to add to the video
+    # Note: ALL audio tracks in the folder will be added, so ensure only the tracks you want are in there
+    # A resulting copy of the original video, now with all tracks added, will also be placed in this folder
 tracksFolder = r"output" 
 
-
-# The video can be anywhere as long as you use the full absolute filepath. Or you can use a relative path.
-# The original will remain the same, and a copy with "MultiTrack" added to the name will be created in the output folder
-# This script assumes the video is an mp4 file. I'm not sure if it will work with other formats/containers.
+    # The video can be anywhere as long as you use the full absolute filepath. Or you can use a relative path.
+    # The original will remain the same, and a copy with "MultiTrack" added to the name will be created in the output folder
+    # This script assumes the video is an mp4 file. I'm not sure if it will work with other formats/containers.
 videoToProcess = r"whatever\path\here"
 
+    # Whether to merge a sound effect track into each audio track before adding to the video
+    # The original audio track files will remain unchanged
+useSoundEffectsTrack = False
 
-# The three letter language code for the default track. English = eng, Spanish = spa, etc
+    # If applicable, the filename of the sound effects or music track to add to each audio track before adding to the video
+    # If "useSoundEffectsTrack" is set to False, this will be ignored
+    # Must be in the same folder as the audio tracks!
+effectsTrackFileName = r"your_sound_effects_file.mp3"
+
+    # Whether to save a copy of each audio track with the sound effects track merged into it
+    # They will go into a folder called "Merged Effects Tracks"
+    # Note: The original audio track files will always remain unchanged no matter this setting
+saveMergedTracks = True
+
+    # The three letter language code for the default track. English = eng, Spanish = spa, etc
 defaultLanguage = "eng" 
 
 #========================================================================================================
@@ -25,14 +37,16 @@ import subprocess as sp
 import os
 import pathlib
 import sys
+import shutil
 # Note: Require ffmpepg to be installed and in the PATH environment variable
 from pydub import AudioSegment
 import langcodes
+from utils import parseBool
 
 # Auto fetch tracks from tracksFolder
 tracksToAddDict = {}
 for file in os.listdir(tracksFolder):
-    if file.endswith(".mp3") or file.endswith(".aac") or file.endswith(".wav"):
+    if (file.endswith(".mp3") or file.endswith(".aac") or file.endswith(".wav")) and file != effectsTrackFileName:
         nameNoExt = os.path.splitext(file)[0]
         # Get the language code from the end of the filename. Assumes the code will be separated by ' - '
         if ' - ' in nameNoExt:
@@ -77,39 +91,131 @@ outputFile = f"{pathlib.Path(videoToProcess).stem} - MultiTrack.mp4"
 
 # Convert each entry in tracksToAddDict to an absolute path and combine with tracksFolder
 tracksFolder = os.path.normpath(tracksFolder)
-tracksToAddDict = {key: os.path.join(tracksFolder, value) for key, value in tracksToAddDict.items()}
+soundEffectsDict = {'effects':effectsTrackFileName}
 videoToProcess = os.path.join(tracksFolder, videoToProcess)
 outputFile = os.path.join(tracksFolder, outputFile)
+tempdir = os.path.join(tracksFolder, "temp")
+mergedTracksDir = os.path.join(tracksFolder, "Merged Effects Tracks")
 
 # Get number of tracks to add
 numTracks = len(tracksToAddDict)
 
-# Check if tracks are stereo, if not it will convert them to stereo before adding
 tempFilesToDelete = []
-for key, value in tracksToAddDict.items():
-    audio = AudioSegment.from_file(value)
-    # Check the number of channels in the audio file
-    num_channels = audio.channels
-    if num_channels == 1:
-        # Get the file extension of the file without the period
-        fileExtension = os.path.splitext(value)[1][1:]
-        # convert to stereo
-        stereo_file = audio.set_channels(2)
-        # save the stereo file
-        tempFileName = f"{value}_stereo_temp.{fileExtension}" # Change this before publishing, needs to adapt to filetype
 
-        # Determine the format needed for pydub to export
-        if fileExtension == "aac":
-            formatString = "adts"
+# Check if tracks are stereo, if not it will convert them to stereo before adding
+def convert_to_stereo(tracksDict):
+    # Key is the language code, value is the relative file path to audio track
+    for langcode, fileName in tracksDict.items():
+        filePath = os.path.join(tracksFolder, fileName)
+
+        audio = AudioSegment.from_file(filePath)
+        # Check the number of channels in the audio file
+        num_channels = audio.channels
+        if num_channels == 1:
+            # Check if temp directory exists, if not create it
+            if not os.path.exists(tempdir):
+                os.makedirs(tempdir)
+            # Get the file extension of the file without the period
+            fileExtension = os.path.splitext(filePath)[1][1:]
+            # convert to stereo
+            stereo_file = audio.set_channels(2)
+            # save the stereo file
+            tempFilePath = f"{os.path.join(tempdir, fileName)}_stereo_temp.{fileExtension}" # Change this before publishing, needs to adapt to filetype
+
+            # Determine the format needed for pydub to export
+            if fileExtension == "aac":
+                formatString = "adts"
+            else:
+                formatString = fileExtension
+
+            # Export the file with appropriate format
+            stereo_file.export(tempFilePath, format=formatString, bitrate="128k") # Change this before publishing, needs to adapt to filetype
+            tracksDict[langcode] = tempFilePath
+
+            # Add to list of files to delete later when done, unless need to save merged tracks
+            if parseBool(useSoundEffectsTrack) and parseBool(saveMergedTracks) and langcode != "effects":
+                pass
+            else:
+                tempFilesToDelete.append(tempFilePath)
+
         else:
-            formatString = fileExtension
+            # File is already stereo, so just use the original file
+            tracksDict[langcode] = filePath
+    return tracksDict
 
-        # Export the file with appropriate format
-        stereo_file.export(tempFileName, format=formatString, bitrate="128k") # Change this before publishing, needs to adapt to filetype
-        tracksToAddDict[key] = tempFileName
-        # Add to list of files to delete later when done
-        tempFilesToDelete.append(tempFileName)
+print("\nChecking if tracks are stereo...")
+tracksToAddDict = convert_to_stereo(tracksToAddDict)
 
+# Use pydub to combine the sound effects track with each audio track
+if parseBool(useSoundEffectsTrack):
+    # Ensure the sound effects track is stereo, if not make it stereo
+    soundEffectsDict = convert_to_stereo(soundEffectsDict)
+
+    # Check if temp directory exists, if not create it
+    if not os.path.exists(tempdir):
+        os.makedirs(tempdir)
+
+    # Check if temporary files for tracks already exist and use those, if not create them
+    for langcode, filePath in tracksToAddDict.items():
+        if "_stereo_temp" not in filePath:
+            # Create the new filename and path for the temporary file
+            fileExtension = os.path.splitext(filePath)[1][1:]
+            fileName = os.path.basename(filePath)
+            appendString = "_temp."+fileExtension
+            tempFilePath = os.path.join(tempdir, fileName+appendString)
+            shutil.copy(filePath, tempFilePath)
+            tracksToAddDict[langcode] = tempFilePath
+            # Add to list of files to delete later when done, unless set to save merged tracks
+            if not parseBool(saveMergedTracks):
+                tempFilesToDelete.append(tempFilePath)
+
+    # Merge the sound effects into temporary track files
+    print("\nMerging sound effects...")
+    for _, trackFilePath in tracksToAddDict.items():
+        soundEffects = AudioSegment.from_file(soundEffectsDict['effects'])
+        audio = AudioSegment.from_file(trackFilePath)
+        combined = audio.overlay(soundEffects)
+        # Double check it is a temporary file
+        if "_temp" in trackFilePath:
+            # Get file extension
+            fileExtension = os.path.splitext(trackFilePath)[1][1:]
+            # Determine the format needed for pydub to export
+            if fileExtension == "aac":
+                formatString = "adts"
+            else:
+                formatString = fileExtension
+            combined.export(trackFilePath, format=formatString, bitrate="128k")
+        else:
+            print("\n\nERROR: The script did not create a temporary file - cannot overwrite original file.")
+            print("This should not happen and is a bug. Please report it here: https://github.com/ThioJoe/Auto-Synced-Translated-Dubs/issues")
+            userInput = input("\nPress Enter to exit... ")
+            sys.exit()
+        
+        # If set to save merged tracks, move the temporary file to the tracks folder
+        if parseBool(saveMergedTracks):
+            # If the merged tracks directory does not exist, create it
+            if not os.path.exists(mergedTracksDir):
+                os.makedirs(mergedTracksDir)
+            # Get the filename from the temporary file path
+            tempFileName = os.path.basename(trackFilePath)
+            ext = os.path.splitext(trackFilePath)[1][1:]
+            # Remove the _temp from the filename, also remove double extensions
+            fileName = fileName.replace("_stereo_temp", "")
+            fileName = tempFileName.replace("_temp", "")
+            fileName = fileName.replace(f".{ext}.{ext}", f".{ext}")
+
+            # Insert effects to the filename before the last " - "
+            nameNoExt = os.path.splitext(fileName)[0]
+            parsedLanguageCode = nameNoExt.split(' - ')[-1].strip()
+            fileName = fileName.replace(parsedLanguageCode, f"With Effects - {parsedLanguageCode}")
+
+            # Create the new file path
+            newFilePath = os.path.join(mergedTracksDir, fileName)
+            # Move the file
+            shutil.move(trackFilePath, newFilePath)
+            # Add the new file path to the tracksToAddDict
+            tracksToAddDict[langcode] = newFilePath
+        
 
 # Create string for ffmpeg command for each string
 #Example:    sp.run(f'ffmpeg -i "video.mp4" -i "audioTrack.mp3" -map 0 -map 1 -metadata:s:a:0 language=eng -metadata:s:a:1 language=spa -codec copy output.mp4')
@@ -118,19 +224,28 @@ trackStringsCombined = ""
 mapList = "-map 0"
 metadataCombined = f'-metadata:s:a:0 language={defaultLanguage} -metadata:s:a:0 title="{defaultLanguage}" -metadata:s:a:0 handler_name="{defaultLanguage}"'
 count = 1
-for key, value in tracksToAddDict.items():
-    trackStringsCombined += f' -i "{value}"'
-    metadataCombined += f' -metadata:s:a:{count} language={key}'
-    metadataCombined += f' -metadata:s:a:{count} handler_name={key}' # Handler shows as the track title in MPC-HC
-    metadataCombined += f' -metadata:s:a:{count} title="{key}"' # This is the title that will show up in the audio track selection menu
+for langcode, filePath in tracksToAddDict.items():
+    trackStringsCombined += f' -i "{filePath}"'
+    metadataCombined += f' -metadata:s:a:{count} language={langcode}'
+    metadataCombined += f' -metadata:s:a:{count} handler_name={langcode}' # Handler shows as the track title in MPC-HC
+    metadataCombined += f' -metadata:s:a:{count} title="{langcode}"' # This is the title that will show up in the audio track selection menu
     mapList += f' -map {count}'
     count+=1
 
 finalCommand = f'ffmpeg -i "{videoToProcess}" {trackStringsCombined} {mapList} {metadataCombined} -codec copy "{outputFile}"'
 
+print("\n Adding audio tracks to video...")
 sp.run(finalCommand)
 
 # Delete temp files
+print("\nDeleting temporary files...")
 for file in tempFilesToDelete:
     os.remove(file)
+# Delete temp directory
+try:
+    if os.path.exists(tempdir):
+        os.rmdir(tempdir)
+except OSError as e:
+    print("Could not delete temp directory. It may not be empty.")
 
+print("\nDone!")
--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 def parseBool(string):
-    if string.lower() == 'true':
-        return True
-    elif string.lower() == 'false':
-        return False
+    if type(string) == str:
+        if string.lower() == 'true':
+            return True
+        elif string.lower() == 'false':
+            return False
+    elif type(string) == bool:
+        if string == True:
+            return True
+        elif string == False:
+            return False
     else:
-        raise ValueError('Not a boolean string')
+        raise ValueError('Not a valid boolean string')

1b480526ea8cbead1802cb70380eba80f1208581
Dalia Sofía Magallón Páramo <magallon.paramo@gmail.com>
2023-01-03 01:08:28 -0600

    Increment release version

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # License: GPLv3
 # NOTE: By contributing to this project, you agree to the terms of the GPLv3 license, and agree to grant the project owner the right to also provide or sell this software, including your contribution, to anyone under any other license, with no compensation to you.
 
-version = '0.7.0'
+version = '0.8.0'
 print(f"------- 'Auto Synced Translated Dubs' script by ThioJoe - Release version {version} -------")
 
 # Import other files

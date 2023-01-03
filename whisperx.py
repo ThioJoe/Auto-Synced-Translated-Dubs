8eed0b8401fcfc4fdf2cf6f6461b542b521ad34a
Joao Victor SDV <jvsdv3@gmail.com>
2023-01-02 21:15:56 -0300

    video location

--- whisperx.py
+++ whisperx.py
@@ -2,3 +2,15 @@ import re
 import subprocess
 import os
 import configparser
+
+#---------------------------------------- Batch File Processing ----------------------------------------
+batchConfig = configparser.ConfigParser()
+batchConfig.read('batch.ini')
+originalVideoFile = os.path.abspath(batchConfig['SETTINGS']['original_video_file_path'].strip("\""))
+
+# MOVE THIS INTO A VARIABLE AT SOME POINT
+outputFolder = "output"
+
+# Get the video file name Create the output folder based on the original video file name
+originalVideoFile = os.path.abspath(batchConfig['SETTINGS']['original_video_file_path'].strip("\""))
+
